f402830d28c872d23e49ac546a593f9bcba33898
bmthd <jougennotuki67@gmail.com>
2025-04-13 21:01:49 +0900

    feat: refactor WeatherPage to use useSearchParams for region retrieval

--- src/views/weather/index.tsx
+++ src/views/weather/index.tsx
@@ -1,15 +1,13 @@
+"use client";
+import { useSearchParams } from "next/navigation";
 import { supportRegionNameMap, supportRegionSchema } from "./schema";
 import { WeatherSection } from "./weather-section";
 
-type SearchParams = { [key: string]: string | string[] | undefined };
-
-export default async function WeatherPage({ searchParams }: { searchParams: Promise<SearchParams> }) {
-	const { region } = await searchParams;
+export default async function WeatherPage() {
+	const searchParams = useSearchParams();
+	const region = searchParams.get("region");
 	if (!region) {
 		return <div>地域が指定されていません</div>;
-	}
-	if (Array.isArray(region)) {
-		return <div>地域は1つだけ指定してください</div>;
 	}
 
 	const parsedRegion = supportRegionSchema.safeParse(region);
