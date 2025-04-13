9c7f5e8d09fab1a22a064aa44f9a0ba3ba06fbd4
bmthd <jougennotuki67@gmail.com>
2025-04-13 20:57:02 +0900

    feat: remove unnecessary blank line in Providers component

--- src/views/shared-layout/providers.tsx
+++ src/views/shared-layout/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import { useState } from "react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
